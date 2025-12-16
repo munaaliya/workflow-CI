955fe1ae7b78ae0d49f6d9635a303960601e275d
munaaliya <munazahra1232019@gmail.com>
2025-12-16 16:14:58 +0700

    Refactor modelling.py to include train_model function

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,3 +1,4 @@
+import argparse
 import mlflow
 import mlflow.sklearn
 import pandas as pd
@@ -12,38 +13,40 @@ from sklearn.metrics import accuracy_score
 # mlflow.autolog() DIHAPUS
 # karena kriteria Advanced mewajibkan manual logging
 
+df train_model(data_path: str):
+    df = pd.read_csv("preprocessing/smart_home_processed.csv")
+    
+    X = df.drop("SmartHomeEfficiency", axis=1)
+    y = df["SmartHomeEfficiency"]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-df = pd.read_csv("preprocessing/smart_home_processed.csv")
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=100,random_state=42)
+        model.fit(X_train, y_train)
 
-X = df.drop("SmartHomeEfficiency", axis=1)
-y = df["SmartHomeEfficiency"]
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+    
+        # Manual logging metric (wajib Advanced)
+        mlflow.log_metric("accuracy", acc)
+    
+        # Log model sebagai artifact "model/"
+        # agar bisa digunakan oleh MLflow Project & Docker
+        mlflow.sklearn.log_model(
+            model,
+            artifact_path="model"
+        )
+        print("Training selesai. Accuracy:", acc)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-with mlflow.start_run():
-
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        required=True,
+        help="Path ke dataset preprocessing"
     )
-    model.fit(X_train, y_train)
+    args = parser.parse_args()
+    train_model(args.data_path)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
-    # Manual logging metric (wajib Advanced)
-    mlflow.log_metric("accuracy", acc)
-
-    # Log model sebagai artifact "model/"
-    # agar bisa digunakan oleh MLflow Project & Docker
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model"
-    )
-
-    # Log parameter manual
-    mlflow.log_param("n_estimators", 100)
-
-    print("Training selesai. Accuracy:", acc)
